6e9b4c24c13dae81e0151df010752d7174414ae6
Eric Dugre <ericd@kentico.com>
2025-06-17 15:46:25 -0400

    add client development support, update to NET 8, update to XbK 30.0.0

---
+++ src/Admin/SqlBrowserModule.cs
@@ -0,0 +1,24 @@
+﻿using Kentico.Xperience.Admin.Base;
+
+using XperienceCommunity.SqlBrowser.Admin;
+
+[assembly: CMS.RegisterModule(typeof(SqlBrowserModule))]
+namespace XperienceCommunity.SqlBrowser.Admin;
+
+/// <summary>
+/// A module that initializes the SQL browser administration components.
+/// </summary>
+internal class SqlBrowserModule : AdminModule
+{
+    public SqlBrowserModule() : base(nameof(SqlBrowserModule))
+    {
+    }
+
+
+    protected override void OnInit()
+    {
+        base.OnInit();
+
+        RegisterClientModule("xperience-community", "sql-browser");
+    }
+}
